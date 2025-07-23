46691d6adf5b2e55f6eb94272297f12c7de427a3
Samir Husain <samir.husain26@gmail.com>
2025-07-22 22:22:43 -0400

    Initial Commit

---
+++ LifeHub/LifeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeHubApp.swift
+//  LifeHub
+//
+//  Created by Samir  Husain on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeHubTests/LifeHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeHubTests.swift
+//  LifeHubTests
+//
+//  Created by Samir  Husain on 7/22/25.
+//
+
+import Testing
+@testable import LifeHub
+
+struct LifeHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
